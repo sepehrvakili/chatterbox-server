07a4ac8cea4b2c07e708be5745a8f26a53b0c45b
sepehrvakili <sdvakili@gmail.com>
2016-05-09 18:28:32 -0700

    Refactor

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,44 +1,8 @@
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
-
 var data = {
   results: []
 };
 
 var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
 
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
@@ -47,7 +11,7 @@ var requestHandler = function(request, response) {
 
   headers['Content-Type'] = 'JSON';
 
-  if ( request.url !== '/classes/messages') {
+  if ( request.url !== '/classes/messages/') {
     response.writeHead(404, headers);
     response.end('This page does not exit');
   }
@@ -71,26 +35,59 @@ var requestHandler = function(request, response) {
 
     request.on('end', function () {
       var post = JSON.parse(body);
-      var postExists = false;
-      for (var i = 0; i < data.results.length; i++) {
-        if ((post.username === data.results[i].username) && (post.message === data.results[i].message)) {
-          postExists = true;
-          break;
-        }
-      }
-
-      if (postExists) {
-        response.writeHead(201, headers);
-        response.end(JSON.stringify(post));
-      } else {
-        response.writeHead(201, headers);
-        data.results.push(post);
-        response.end();
-      }
-
-    // end of request.on('end',...)
+      response.writeHead(201, headers);
+      data.results.push(post);
+      response.end(JSON.stringify(post));
     });
   }
+
+};
+
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
+
+exports.requestHandler = requestHandler;
+
+/*************************************************************
+
+You should implement your request handler function in this file.
+
+requestHandler is already getting passed to http.createServer()
+in basic-server.js, but it won't work as is.
+
+You'll have to figure out a way to export this function from
+this file and include it in basic-server.js so that it actually works.
+
+*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
+
+**************************************************************/
+
+ // Request and Response come from node's http module.
+  //
+  // They include information about both the incoming request, such as
+  // headers and URL, and about the outgoing response, such as its status
+  // and content.
+  //
+  // Documentation for both request and response can be found in the HTTP section at
+  // http://nodejs.org/documentation/api/
+
+  // Do some basic logging.
+  //
+  // Adding more logging to your server can be an easy way to get passive
+  // debugging help, but you should always be careful about leaving stray
+  // console.logs in your code.
+
+  // .writeHead() writes to the request line and headers of the response,
+  // which includes the status and all headers.
+
+  // Tell the client we are sending them plain text.
+  //
+  // You will need to change this if you are sending something
+  // other than plain text, like JSON or HTML.
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -99,9 +96,6 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-
-};
-
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
 // are on different domains, for instance, your chat client.
@@ -111,11 +105,3 @@ var requestHandler = function(request, response) {
 //
 // Another way to get around this restriction is to serve you chat
 // client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
-
-exports.requestHandler = requestHandler;
